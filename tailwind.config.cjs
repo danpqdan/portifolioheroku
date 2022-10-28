a8333e0eac41a895d143991a04f3631bac654dd9
Daniel Santos <danieltisantos@gmail.com>
2022-10-28 17:39:38 -0300

    atualizado digner system com figma

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontSize:{
+      'hd-lg': 32,
+      'hd-md': 28,
+      'hd-sm': 24,
+      'tx-lg': 20,
+      'lx-md': 16
+    },
+
+    colors:{
+      white: '#fff',
+      black: '#000',
+
+      'pink-900': '#FB2576',
+      'pink-500': '#F35490',
+
+      'blue-900': '#150050',
+      'blue-500': '#492AA0',
+    },
+    extend: {
+      fontFamily:{
+        'soon': 'Comming Soon', sans,
+        'consolas': 'Consolas', sans,
+      }
+    },
   },
   plugins: [],
 }
